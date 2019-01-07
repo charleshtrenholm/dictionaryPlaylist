81c9319dda7c2724492b20dfb6bb4a37aedffb91
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-01-06 16:38:15 -0800

    first commit

---
+++ apps/streaming/models.py
@@ -0,0 +1,43 @@
+from __future__ import unicode_literals
+from django.db import models
+import re
+
+class UserManager(models.Manager):
+    def checkRegistration(self, data):
+        emailRegex = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+        userEmails = User.objects.all()
+        errors = {}
+        if len(data['first_name']) < 2 or len(data['last_name']) < 2:
+            errors['name'] = "Sorry: email must contain at least 2 characters"
+        elif bool(re.search(r'\d', data['first_name'])) == True:
+            errors['name'] = "Sorry: name must not contain a number"
+        elif bool(re.search(r'\d', data['last_name'])) == True:
+            errors['name'] = "Sorry name must not contain a number"
+        elif not emailRegex.match(data['email']):
+            errors['email'] = "Sorry: invalid email address"
+        for email in userEmails:
+            if data['email'] == email.email:
+                errors['email'] = "Sorry: email already in use!"
+        if bool(re.search(r'\d', data['password'])) == False:
+            errors['password'] = "Sorry: Password must contain at least 1 number"
+        elif len(data['password']) < 8:
+            errors['password'] = "Sorry: Password Must contain at least 8 Characters"
+        elif data['password'] != data['pw_confirm']:
+            errors['password'] = "Sorry: Password and confirmation do not match"
+        return errors
+        
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 255)
+    email_option = models.CharField(max_length = 15)
+    objects = UserManager()
+    def __repr__(self):
+        return "<user first name: {}, last name: {}, email: {}, email option: {}>".format(self.first_name, self.last_name, self.email, self.email_option)
+
+class Playlist(models.Model):
+    name = models.CharField(max_length = 255)
+
+# Create your models here.
---
+++ apps/streaming/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, redirect
+from bs4 import BeautifulSoup, Tag, NavigableString
+import re, requests, bcrypt
+from .models import User, Playlist
+from django.contrib import messages
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+client_credentials_manager = SpotifyClientCredentials(client_id ="0073769af7114279846e60afe97cd66", client_secret ="bd3913da8e2f4c8db59503e69bf77fc6")
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def index(request):
+    url = 'https://www.merriam-webster.com/word-of-the-day'
+    response = requests.get(url)
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+    word = soup.h1.string
+    defTable = soup.find('div', attrs = {'class' : 'wod-definition-container'})
+
+    if 'login' not in request.session:
+        request.session['login'] = False
+
+    defs = ''
+    runner = defTable.find('h2')
+
+    while runner.next_element != 'Did You Know?':
+        if isinstance(runner, Tag):
+            defs += str(runner)
+            if runner.children:
+                for child in runner.children:
+                    if not isinstance(runner.next_element, Tag):
+                        runner = runner.next_element.next_element
+                    else:
+                        runner = runner.next_element
+        runner = runner.next_element
+
+    print (isinstance(word, str))
+
+    # results = sp.search(q="songs:" + word, limit = 20, type="song")
+    # print(results)
+
+    context = {
+        'word' : word,
+        'defs' : defs
+    }
+
+    return render(request, 'streaming/index.html', context)
+
+def create(request):
+    if 'word' not in request.session:
+        request.session['word'] = request.POST['word']
+    else:
+        request.session['word'] = request.POST['word']
+    return redirect('/result')
+
+def result(request):
+    context = {
+        'word' : request.session['word']
+    }
+    return render(request, 'streaming/result.html', context)
+
+def signup(request):
+    errors = User.objects.checkRegistration(request.POST)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/#userSignup')
+    else:
+        pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+        User.objects.create(first_name = request.POST['first_name'],
+                            last_name = request.POST['last_name'],
+                            email = request.POST['email'],
+                            password = pw_hash.decode('utf-8'),
+                            email_option = request.POST['email_option'])
+        request.session['login'] = True
+        if 'id' not in request.session:
+            request.session['id'] = User.objects.get(email = request.POST['email']).id
+        return redirect('/success')
+
+def success(request):
+    context = {
+        'loggedUser' : User.objects.get(id=request.session['id']),
+        'users' : User.objects.all()
+    }
+
+    return render(request, "streaming/success.html", context)
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+    
+                            
